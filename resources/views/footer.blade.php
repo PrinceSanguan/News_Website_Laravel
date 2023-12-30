3486cc387dcad860cab22557f121cdbe8b216764
Prince Sanguan <princesanguan44@gmail.com>
2023-12-30 13:18:43 +0800

    adding controller

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,7 @@
 <!-- END #fh5co-Footer -->
 <footer id="fh5co-footer">
-  <p><small>&copy; 2016. Magazine Free HTML5. All Rights Reserverd. <br> Designed by <a href="http://freehtml5.co" target="_blank">FREEHTML5.co</a>  Demo Images: <a href="http://unsplash.com/" target="_blank">Unsplash</a></small></p>
+  <p><small>&copy; 2016. Magazine Free HTML5. All Rights Reserverd. <br>
+  <a href="{{url('login')}}">Login</a> -- <a href="{{url('register')}}" >Register</a></small></p>
 </footer>
 
 

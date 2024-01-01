f746e9a925de93068cff06f88d27344bf8d5a84a
Prince Sanguan <princesanguan44@gmail.com>
2024-01-01 15:05:00 +0800

    Unang commit sa taong ito!

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,23 +4,47 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class AdminController extends Controller
 {
     //
 
-    public function index(Request $req) 
-    {
-        return view('admin.admin',['page_title'=>'Dashboard']);
-    }
-
-    public function posts(Request $req, $type ='')
-    {
-        switch ($type) {
-            case 'add':
-                if ($req->method() == 'POST'){
-                   $req->file('file')->store('/', ['disks'=>'my_disk']);
+                public function index(Request $req) 
+                {
+                    return view('admin.admin',['page_title'=>'Dashboard']);
                 }
+
+                public function posts(Request $req, $type = '')
+                {   
+                switch ($type) {
+                    case 'add':
+                    if ($req->isMethod('post')) {
+
+                        $validated = $req->validate([
+                            'title' => 'required|string',
+                            'file' => 'required|image',
+                            'content' => 'required'
+                        ]);
+
+                if ($req->hasFile('file') && $req->file('file')->isValid()) {
+
+                    $post = new Post();
+
+                    $path = $req->file('file')->store('/', ['disk' => 'my_disk']);
+
+                    $data = [
+                        'title' => $req->input('title'),
+                        'category_id' => 1,
+                        'image' => $path,
+                        'content' => $req->input('content'),
+                        'created_at' => date("Y-m-d H:i:s"),
+                        'updated_at' => date("Y-m-d H:i:s")
+                    ];
+
+                    $post->insert($data);
+                }
+            }
 
                 return view('admin.add_post',['page_title'=>'New Posts']);
                 break;

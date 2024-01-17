4499b990aa59e3352f8c39691c1c071a96c099be
Prince Sanguan <princesanguan44@gmail.com>
2024-01-17 21:16:44 +0800

    fixing view single page

--- app/Http/Controllers/SingleController.php
+++ app/Http/Controllers/SingleController.php
@@ -9,15 +9,33 @@ class SingleController extends Controller
 {
     //
 
-    public function index(Request $req) 
-    {
+    public function index(Request $req, $id = '') 
+{
+    $query = "select * from posts where slag = :slag limit 1";
+    $row = DB::select($query, ['slag' => $id]);
 
-        $query = "select * from categories order by id desc";
-        $categories = DB::select($query);
+    $data = [];
 
-        $data['categories'] = $categories;
-        return view('single', $data);
+    if ($row) {
+        $postId = $row[0]->id;
+
+        $query = "select * from categories where id = :id limit 1";
+        $category = DB::select($query, ['id' => $postId]);
+
+        // Check if the category is found before accessing its index
+        if ($category) {
+            $data['row'] = $row[0];
+            $data['category'] = $category[0];
+        }
     }
+
+    $query = "select * from categories order by id desc";
+    $categories = DB::select($query);
+
+    $data['categories'] = $categories;
+
+    return view('single', $data);
+}
 
     public function save(Request $req) 
     {
